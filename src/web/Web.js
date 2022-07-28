d4421c7584bfe140ed8a85407e8487b3ca088082
Tim Wilkinson <tim.j.wilkinson@gmail.com>
2022-07-27 19:18:21 -0700

    Speed

--- src/web/Web.js
+++ src/web/Web.js
@@ -1,10 +1,11 @@
 
 const Log = require("debug")("web");
 
-const Koa = require('koa');
-const Websockify = require('koa-websocket');
-const Router = require('koa-router');
-const Pages = require('./Pages');
+const Koa = require("koa");
+const KoaCompress = require("koa-compress");
+const Websockify = require("koa-websocket");
+const Router = require("koa-router");
+const Pages = require("./Pages");
 
 // Web port
 const webport = parseInt(process.env.PORT || 8080);
@@ -14,18 +15,19 @@ const Web = {
     run() {
         Log("running:");
         const Web = Websockify(new Koa());
-        Web.on('error', err => console.error(err));
+        Web.on("error", err => console.error(err));
 
         const root = Router();
         const wsroot = Router();
 
         Pages(root, wsroot);
 
+        Web.use(KoaCompress());
         Web.use(root.middleware());
         Web.ws.use(wsroot.middleware());
         Web.ws.use(async (ctx, next) => {
         await next(ctx);
-        if (ctx.websocket.listenerCount('message') === 0) {
+        if (ctx.websocket.listenerCount("message") === 0) {
             ctx.websocket.close();
         }
         });
